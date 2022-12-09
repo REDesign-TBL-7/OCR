3fa67baa0a330fc77929d2f8c52ea3ec50572440
emmelyn <emmylynx@gmail.com>
2022-12-10 01:14:25 +0800

    first commit

---
+++ ocrtest.py
@@ -0,0 +1,18 @@
+import cv2
+import pytesseract
+import numpy as np
+from pytesseract import Output
+ 
+img =cv2.imread('images/5.jpg')
+
+d = pytesseract.image_to_data(img, output_type=Output.DICT)
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 60:
+        (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        # don't show empty text
+        if text and text.strip() != "":
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            img = cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)
+cv2.imshow('img', img)
+cv2.waitKey(0)
