e45a1e45e859b4c93df85eed3b8142d4e3103f19
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-03-03 01:35:02 +0800

    Fix motor.py

--- modules/motor.py
+++ modules/motor.py
@@ -4,80 +4,77 @@ from adafruit_motorkit import MotorKit
 import board
 
 down = True
-turning = [0] * 10
-
-def execute_motor_instructions(motor_instruction, motor_id):
-  global down
-  count = 0
-  while True:
-    if len(motor_instruction) == 0:
-      print(f"Motor {motor_id} Terminating")
-      break
-    if down and motor_instruction[0] == "0":
-      motor_instruction = motor_instruction[1:]
-      count += 1
-      turn_motor(motor_id, "F")
-      print(f"Motor {motor_id} turning..., Count: {count}, Motor Instruction: {motor_instruction}")
-    elif not down and motor_instruction[0] == "1":
-      motor_instruction = motor_instruction[1:]
-      count += 1
-      turn_motor(motor_id, "F")
-      print(f"Motor {motor_id} turning..., Count: {count}, Motor Instruction: {motor_instruction}")
-
+kit1 = MotorKit(address=0x60)
+kit2 = MotorKit(address=0x61)
+kit3 = MotorKit(address=0x62)
+kit4 = MotorKit(address=0x63)
 
 def send_motor_instructions(motor_instructions):
-  global down, turning
+  global down
   print(f"Motor Instructions: {motor_instructions}")
 
-  for i in range(len(motor_instructions) // 6):
-    # motor_t1 = threading.Thread(target=execute_motor_instructions, args=(motor_instructions[i * 6], 0))
-    # motor_t2 = threading.Thread(target=execute_motor_instructions, args=(motor_instructions[i * 6 + 1], 1))
-    # motor_t3 = threading.Thread(target=execute_motor_instructions, args=(motor_instructions[i * 6 + 2], 2))
-    # motor_t4 = threading.Thread(target=execute_motor_instructions, args=(motor_instructions[i * 6 + 3], 3))
-    motor_t5 = threading.Thread(target=execute_motor_instructions, args=(motor_instructions[i * 6 + 4], 8))
-    motor_t6 = threading.Thread(target=execute_motor_instructions, args=(motor_instructions[i * 6 + 5], 9))
+  motors_executed_count = 0
+  while motors_executed_count != 6:
+    print(motor_instructions)
+    motors_done_count = 0
+    while motors_done_count != 6:
+      motors_done_count = 0
+      motors_to_turn = {
+        0: False,
+        1: False,
+        2: False,
+        3: False,
+        4: False,
+        5: False,
+        6: False
+      }
+      for i in range(len(motor_instructions)):
+        motor_instruction = motor_instructions[i]
+        if motor_instruction == "" or down and motor_instruction[0] == "1" or not down and motor_instruction[0] == "0":
+          motors_done_count += 1
+          continue
+        if down and motor_instruction[0] == "0":
+          motors_to_turn[i] = True
+          motor_instructions[i] = motor_instruction[1:]
+        if not down and motor_instruction[0] == "1":
+          motors_to_turn[i] = True
+          motor_instructions[i] = motor_instruction[1:]
+        if motor_instructions[i] == "":
+          motors_executed_count += 1
 
-    # motor_t1.start()
-    # motor_t2.start()
-    # motor_t3.start()
-    # motor_t4.start()
-    motor_t5.start()
-    motor_t6.start()
+      turn_motors(motors_to_turn)
 
-  while (motor_t5.is_alive() or motor_t6.is_alive()):
-    time.sleep(20)
     if down:
       print("Raising elevator")
-      turn_motor(6, "F")
+      turn_elevator_motor()
       down = False
     else:
       print("Lowering elevator")
-      turn_motor(6, "B")
+      turn_elevator_motor(direction="B")
       down = True
-  
+
   print("End")
-  
-def turn_motor(motor_id, direction="F"):
-    global turning
-    address = motor_id // 2 + 0x60
-    
-    print(turning)
-    # Enable only 0x64 for testing purposes
-    if address != 0x64:
-        return
-    turning[motor_id] = 1
-    kit = MotorKit(address=address)
-    print(f"Motor {motor_id} actually turning")
-    for i in range(291):
-        if (motor_id % 2 == 0):
-            kit.stepper1.onestep()
-        else:
-            kit.stepper2.onestep()
-    turning[motor_id] = 0
-    print(f"Motor {motor_id} actually done turning")
+  exit()
+
+def turn_elevator_motor(direction="F"):
+  for i in range(291):
+    kit4.stepper1.onestep(direction=direction)
+
+def turn_motors(motor_ids):
+  for i in range(291):
+    if (motor_ids[0]):
+      kit1.stepper1.onestep()
+    if (motor_ids[1]):
+      kit1.stepper2.onestep()
+    if (motor_ids[2]):
+      kit2.stepper1.onestep()
+    if (motor_ids[3]):
+      kit2.stepper2.onestep()
+    if (motor_ids[4]):
+      kit3.stepper1.onestep()
+    if (motor_ids[5]):
+      kit3.stepper2.onestep()
 
 if __name__ == "__main__":
   motor_instructions = ['001010', '001110', '011010', '011001', '000111', '111000']
   send_motor_instructions(motor_instructions)
-  #turn_motor(8)
-  #turn_motor(9)
---
+++ modules/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+kit1 = MotorKit(address=0x60)
+# kit2 = MotorKit(address=0x61)
+# kit3 = MotorKit(address=0x62)
+# kit4 = MotorKit(address=0x63)
