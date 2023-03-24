9168338accecf655e4c8b486eaa39a6816568a3f
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-03-24 14:30:07 +0800

    Test Elevators

--- modules/stepper_test.py
+++ modules/stepper_test.py
@@ -1,13 +1,14 @@
 import time
 import board
 from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
 
 kit = MotorKit(address=0x64)
 
 def stepper_test():
     for i in range(7):
         for j in range(291):
-            kit.stepper1.onestep()
+            kit.stepper1.onestep(direction=stepper.BACKWARD)
             kit.stepper2.onestep()
         time.sleep(1)
 
