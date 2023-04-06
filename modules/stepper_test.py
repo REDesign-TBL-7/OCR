3342ac1e077e994673c1e78df0f16b542def831c
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-04-06 21:53:52 +0800

    Redo Stepper Test

--- modules/stepper_test.py
+++ modules/stepper_test.py
@@ -5,16 +5,18 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-b", "--backward", action="store_true", help="direction of the motors")
-parser.add_argument("-s", "--steps", type=int, help="number of steps to turn (One revolution = 2037)")
-parser.add_argument("-id", "--id", nargs="*", type=int, help="IDs of Motors to turn")
-parser.add_argument("-d", "--double", action="store_true", help="stepper motor Mode")
+parser.add_argument("--steps", type=int, help="number of steps to turn (One revolution = 2037)")
+parser.add_argument("-d", "--double", nargs="*", type=int, help="IDs of Motors to double turn")
+parser.add_argument("-s", "--single", nargs="*", type=int, help="IDs of Motors to single turn")
+parser.add_argument("-m", "--micro", nargs="*", type=int, help="IDs of Motors to micro turn")
 
 args = parser.parse_args()
 
-motor_list = [x for x in args.id if x >= 0 and x <=7]
-motor_set = set(args.id)
+d_motor_set = set(args.double) if args.double else set()
+s_motor_set = set(args.single) if args.single else set()
+m_motor_set = set(args.micro) if args.micro else set()
 
-style = stepper.DOUBLE if args.double else stepper.SINGLE
+# style = stepper.DOUBLE if args.double else stepper.SINGLE
 direction = stepper.BACKWARD if args.backward else stepper.FORWARD
 
 kit1 = MotorKit(address=0x60)
@@ -22,24 +24,29 @@ kit2 = MotorKit(address=0x61)
 kit3 = MotorKit(address=0x62)
 kit4 = MotorKit(address=0x63)
 
+def turn_stepper(motor_set, style):
+    if 0 in motor_set:
+        kit1.stepper1.onestep(direction=direction, style=style)
+    if 1 in motor_set:
+        kit1.stepper2.onestep(direction=direction, style=style)
+    if 2 in motor_set:
+        kit2.stepper1.onestep(direction=direction, style=style)
+    if 3 in motor_set:
+        kit2.stepper2.onestep(direction=direction, style=style)
+    if 4 in motor_set:
+        kit3.stepper1.onestep(direction=direction, style=style)
+    if 5 in motor_set:
+        kit3.stepper2.onestep(direction=direction, style=style)
+    if 6 in motor_set:
+        kit4.stepper1.onestep(direction=direction, style=style)
+    if 7 in motor_set:
+        kit4.stepper2.onestep(direction=direction, style=style)
+
 def stepper_test():
     for i in range(770 if args.steps == None else args.steps):
-        if 0 in motor_set:
-            kit1.stepper1.onestep(direction=direction, style=style)
-        if 1 in motor_set:
-            kit1.stepper2.onestep(direction=direction, style=style)
-        if 2 in motor_set:
-            kit2.stepper1.onestep(direction=direction, style=style)
-        if 3 in motor_set:
-            kit2.stepper2.onestep(direction=direction, style=style)
-        if 4 in motor_set:
-            kit3.stepper1.onestep(direction=direction, style=style)
-        if 5 in motor_set:
-            kit3.stepper2.onestep(direction=direction, style=style)
-        if 6 in motor_set:
-            kit4.stepper1.onestep(direction=direction, style=style)
-        if 7 in motor_set:
-            kit4.stepper2.onestep(direction=direction, style=style)
-
+        turn_stepper(s_motor_set, stepper.SINGLE)
+        turn_stepper(d_motor_set, stepper.DOUBLE)
+        turn_stepper(m_motor_set, stepper.MICROSTEP)
+        
 if __name__ == "__main__":
     stepper_test()
