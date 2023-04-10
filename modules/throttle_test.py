38dbbe374743ad09ba2d3c46fdc630448d97247a
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-04-11 01:06:14 +0800

    Redo throttle test

--- modules/throttle_test.py
+++ modules/throttle_test.py
@@ -6,12 +6,16 @@ import time
 import board
 from adafruit_motorkit import MotorKit
 
-
+kit1 = MotorKit(address=0x60)
+kit2 = MotorKit(address=0x61)
+kit3 = MotorKit(address=0x62)
 kit4 = MotorKit(address=0x63)
 
-while True:
-  kit4.motor1.throttle = 1.0
-  kit4.motor2.throttle = 1.0
-  time.sleep(0.5)
-  kit4.motor1.throttle = 0
-  kit4.motor2.throttle = 0
+kit1.stepper1.throttle = 1
+kit1.stepper2.throttle = 1
+kit2.stepper1.throttle = 1
+kit2.stepper2.throttle = 1
+kit3.stepper1.throttle = 1
+kit3.stepper2.throttle = 1
+kit4.stepper1.throttle = 1
+kit4.stepper2.throttle = 1
