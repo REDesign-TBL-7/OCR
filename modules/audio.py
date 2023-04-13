58168e4e7b4ba1368b3fe75efba8e60e0b6f44c9
Chay Hui Xiang <chayhuixiang@gmail.com>
2023-04-14 02:40:22 +0800

    Change audio

--- modules/audio.py
+++ modules/audio.py
@@ -1,9 +1,33 @@
 import serial
-import os
 
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+# Open a serial connection to the DFPlayer Mini
+ser = serial.Serial('/dev/ttyS0', 9600)
 
-def play_sound(sound_number):
-    ser.write(bytearray([0x7E, 0xFF, 0x06, 0x03, 0x00, sound_number, 0x00, 0x00, 0xEF]))
+# Send a command to the DFPlayer Mini
+def send_cmd(cmd, feedback=True):
+    # Create a bytearray with the command and checksum
+    data = bytearray([0x7e, 0xff, 0x06, cmd >> 8, cmd & 0xff, 0x00, 0x00, 0x00, 0xef])
 
-play_sound(1) # play the first sound on the DFPlayer Mini
+    # Send the command to the DFPlayer Mini
+    ser.write(data)
+
+    # Wait for feedback if enabled
+    if feedback:
+        while True:
+            if ser.read() == b'\x7e':
+                if ser.read() == b'\xff':
+                    if ser.read() == b'\x06':
+                        if ser.read() == b'\x00':
+                            if ser.read() == b'\x00':
+                                if ser.read() == b'\x00':
+                                    if ser.read() == b'\xfe':
+                                        if ser.read() == b'\xef':
+                                            break
+
+# Set the volume to 50%
+send_cmd(0x0605, feedback=False)
+send_cmd(0x0601, feedback=False)
+send_cmd(0x0600, feedback=False)
+
+# Play track 001
+send_cmd(0x0301)
